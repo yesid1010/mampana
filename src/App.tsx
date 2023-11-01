356052a974fd1837fea92fa60ce226fb6f8ec828
Yesid Sánchez Ramos <sanchezyesid72@gmail.com>
2023-11-01 15:10:33 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import menu from '/assets/images/menu.jpg'
+import logo from '/assets/images/logo.jpg'
 import './App.css'
 
 function App() {
 
   return (
 
-      
-    <div className="imagen-container">
-    <img src={menu} alt="Descripción de la imagen" className="imagen-responsive"/>
-  </div>
-       
-
+      <>
+    <div className="logo">
+        <img src="https://via.placeholder.com/200x100?text=YOUR+LOGO" alt="Logo">
+    </div>
+    <h1>DRINKS MENU</h1>
+    <div className="menu-container">
+        <div className="menu-section">
+            <div className="drink-item">
+                <span>Lorem ipsum dolor</span>
+                <span className="price">$10</span>
+            </div>
+            <!-- Repite los elementos .drink-item según tus necesidades -->
+        </div>
+        <div className="menu-section">
+            <div className="drink-item">
+                <span>Lorem ipsum dolor</span>
+                <span className="price">$10</span>
+            </div>
+            <!-- Repite los elementos .drink-item según tus necesidades -->
+        </div>
+    </div>
+    <div className="footer">
+        FOR DELIVERY <span className="website">www.website.com</span>
+    </div>
+</>
   )
 }
 
